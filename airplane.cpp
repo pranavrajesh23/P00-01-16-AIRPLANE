eaa92b5a8e6b84d37a5028fbecdd4b469aef35fc
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 15:55:50 +0530

    screening function

---
+++ airplane.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ 
+void screening()
+{
+    cout<<"\n--------Screening Process--------\n";
+    cout<<"\nSecurity Screening\n";
+    cout<<"\nScreening totally has three checks\n";
+    // articleCheck();
+    // metalCheck();
+    // GoodsCheck();
+    cout<<"\nAll screening checks are completed\n";
+    cout<<"\nBe ready to fly high\n";
+    // immigration();
+}
+
+
+int main()
+{
+    cout<<"------Airplane Passenger Workflow------\n";
+    cout<<"\nCheck in\n";
+    cout<<"\nBaggage drop\n";
+    cout<<"\nOut for baggage screening\n";
+    screening();
+}
