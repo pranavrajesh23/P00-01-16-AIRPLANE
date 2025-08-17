b3729f39c6617f73e97e71fd4e87b2ec23c2e651
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 16:00:47 +0530

    article check

--- airplane.cpp
+++ airplane.cpp
@@ -1,12 +1,30 @@
 #include<iostream>
 using namespace std;
  
+void articleCheck()
+{
+    cout<<"\n--------Article Check--------\n";
+    cout<<"\nDo the baggage contain any restricted article? (yes/no)\n";
+    string art;
+    cin>>art;
+    if(art=="yes")
+    {
+        cout<<"\nGive up the articles above permitted level\n";
+    }
+    else
+    {
+        cout<<"\nNo restricted article found\n";
+    }
+}
+
 void screening()
 {
     cout<<"\n--------Screening Process--------\n";
     cout<<"\nSecurity Screening\n";
     cout<<"\nScreening totally has three checks\n";
-    // articleCheck();
+    cout<<"\nRedirecting to the initial check\n";
+    articleCheck();
+    cout<<"\nRedirecting to the next check\n";
     // metalCheck();
     // GoodsCheck();
     cout<<"\nAll screening checks are completed\n";
